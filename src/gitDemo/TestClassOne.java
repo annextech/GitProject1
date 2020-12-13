041513046ff3da4fc3d36055c77cba6ba3493a2c
nawshad <mohammad.bhuiyan77@gmail.com>
2020-12-13 17:01:43 -0500

    First Commit

---
+++ src/gitDemo/TestClassOne.java
@@ -0,0 +1,16 @@
+package gitDemo;
+
+public class TestClassOne {
+
+	public static void main(String[] args) {
+		method1();
+		method2();		
+	}	
+	public static void method1() {
+		System.out.println("method1");
+	}
+	
+	public static void method2() {
+		System.out.println("method2");
+	}	
+}
